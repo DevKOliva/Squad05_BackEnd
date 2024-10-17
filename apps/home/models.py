42bf171cd5dc1859cb456acf909b80dd10eb5f5a
MCipris <cipriano.cn.acad128@gmail.com>
2024-10-16 21:13:23 -0300

    feat: projeto configurado com a parte inicial do front end, e adição dos apps home e forum.

---
+++ apps/home/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Ferramenta(models.Model):
+    nome = models.CharField(verbose_name='Nome da ferramenta', max_length=50)
+    descricao = models.CharField(verbose_name='Descrição da ferramenta', max_length=50)
+
+class Profissional(models.Model):
+    nome = models.CharField(verbose_name='Nome do profissional', max_length=100)
+    profissao = models.CharField(verbose_name='Nome da profissão', max_length=50)
